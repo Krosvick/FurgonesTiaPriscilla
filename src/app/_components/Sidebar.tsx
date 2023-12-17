3d0c73a5f86bba4f728c4770a9a6e8a6d4212dd3
José Sandoval <josesand99v@gmail.com>
2023-12-17 02:18:53 +0000

    Refactor Sidebar component styles

--- src/app/_components/Sidebar.tsx
+++ src/app/_components/Sidebar.tsx
@@ -11,16 +11,16 @@ export function Sidebar({sidebarItems, sidebarButton, children}: SidebarProps) {
     return (
         <aside className="drawer lg:drawer-open">
             <input id="my-drawer-2" className="drawer-toggle" type="checkbox" />
-            <div className="drawer-content flex items-center">
+            <div className="drawer-content">
                 {sidebarButton}
-                <div className="drawer-content min-h-screen min-w-screen flex items-start justify-start p-5">
+                <div className="min-h-screen min-w-screen flex items-start justify-start p-5">
                     {children}
                 </div>
             </div>
             <div className="drawer-side">
                 <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                <div className="flex items-center mt-5 ml-5 ">
-                    <ul className="menu min-h-fit rounded-lg shadow-lg bg-gray-700 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-60 text-gray-200 font-medium">
+                <div className="flex items-center mt-5 lg:mx-5">
+                    <ul className="menu rounded-lg shadow-lg bg-gray-700 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-60 text-gray-200 font-medium">
                         {sidebarItems?.map((item, index) => (
                             <li key={index}>
                                 {item}
