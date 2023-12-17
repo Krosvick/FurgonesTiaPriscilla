f55609c7dde9de8f2c370d89a6f0df9473021243
José Sandoval <josesand99v@gmail.com>
2023-12-17 02:18:31 +0000

    Refactor Container component to accept background color as a prop

--- src/app/_components/Container.tsx
+++ src/app/_components/Container.tsx
@@ -2,12 +2,13 @@
 
 interface ContainerProps{
     children: React.ReactNode
+    bgColor: string
 }
 
-export function Container(props: ContainerProps){
+export function Container({bgColor, children}: ContainerProps){
     return(
-        <div className='mx-auto p-5 mb-8 md:mb-20 w-full rounded-lg shadow-lg bg-gray-200 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-40'>
-            {props.children}
+        <div className={`mx-auto p-5 mb-8 md:mb-20 w-full rounded-lg shadow-lg ${bgColor} bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-40`}>
+            {children}
         </div>
     )
 }
