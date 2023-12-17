969de3af72ead8d07caf6036bb1b26147efea679
José Sandoval <josesand99v@gmail.com>
2023-12-17 05:01:58 +0000

    Add className prop to Container component

--- src/app/_components/Container.tsx
+++ src/app/_components/Container.tsx
@@ -3,11 +3,12 @@
 interface ContainerProps{
     children: React.ReactNode
     bgColor: string
+    className?: string
 }
 
-export function Container({bgColor, children}: ContainerProps){
+export function Container({bgColor, children, className}: ContainerProps){
     return(
-        <div className={`mx-auto p-5 mb-8 md:mb-20 w-full rounded-lg shadow-lg ${bgColor} bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-40`}>
+        <div className={`mx-auto p-5 mb-8 md:mb-20 w-full shadow-lg ${bgColor} bg-clip-padding backdrop-filter backdrop-blur-md ${className}`}>
             {children}
         </div>
     )
