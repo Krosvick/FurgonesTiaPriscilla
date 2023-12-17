8997d447dc82342be2d2c160c91e22acaf25c11a
José Sandoval <josesand99v@gmail.com>
2023-12-17 05:02:12 +0000

    Update Sidebar styling

--- src/app/_components/Sidebar.tsx
+++ src/app/_components/Sidebar.tsx
@@ -19,8 +19,8 @@ export function Sidebar({sidebarItems, sidebarButton, children}: SidebarProps) {
             </div>
             <div className="drawer-side">
                 <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                <div className="flex items-center mt-5 lg:mx-5">
-                    <ul className="menu rounded-lg shadow-lg bg-gray-700 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-60 text-gray-200 font-medium">
+                <div className="flex items-center mt-40 lg:mt-5 lg:ml-5">
+                    <ul className="menu rounded-lg shadow-lg bg-zinc-50 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 text-gray-800 font-medium">
                         {sidebarItems?.map((item, index) => (
                             <li key={index}>
                                 {item}
