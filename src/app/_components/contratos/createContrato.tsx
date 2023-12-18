350207f9aa996e4364e13f1c7cf49281d362fc41
José Sandoval <josesand99v@gmail.com>
2023-12-18 20:50:43 +0000

    Add form steps and create functionality for contrato

--- src/app/_components/contratos/createContrato.tsx
+++ src/app/_components/contratos/createContrato.tsx
@@ -4,49 +4,109 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import { api } from "~/trpc/react";
 import { useRouter } from "next/navigation";
-import { contratoSchema } from "~/server/zodTypes/contratoTypes";
+import { useState } from "react";
+import { useRut } from "react-rut-formatter";
+import { contratoFrontendSchema } from "~/server/zodTypes/contratoTypes";
+import { apoderadoSchema } from "~/server/zodTypes/apoderadoTypes";
 import Form from "~/app/_components/Form"
 
-type Contrato = z.infer<typeof contratoSchema>;
+type Contrato = z.infer<typeof contratoFrontendSchema>;
+type Apoderado = z.infer<typeof apoderadoSchema>;
 
 export function CreateContrato() {
+    const [step, setStep] = useState(1);
+    const [contrato, setContrato] = useState<Contrato>();
+    const [apoderado, setApoderado] = useState<Apoderado>();
+    const { rut, updateRut, isValid } = useRut();
 
-    const methods = useForm<Contrato>({
-        resolver: zodResolver(contratoSchema),
+    const methods = useForm<Contrato | Apoderado>({
+        resolver: zodResolver(step == 1 ? contratoFrontendSchema : apoderadoSchema),
     });
 
     const router = useRouter();
 
     
 
-    const createApoderado = api.apoderados.create.useMutation({
+    const createContrato = api.contratos.create.useMutation({
         onSuccess: () => {
-            router.refresh();
+            console.log("success")
         },
     });
 
-    const onSubmit: SubmitHandler<Contrato> = async (data) => {
-       try{
-        await createApoderado.mutateAsync(data);
-       } catch (error) {
-              console.log(error);
+    const createApoderado = api.apoderados.create.useMutation({
+        onSuccess: () => {
+            //router push to /admin/contratos/${contrato.idContrato}
+            router.push(`/admin/contratos/${contrato?.idContrato}`);
+        },
+    });
+
+    const onSubmit: SubmitHandler<Contrato | Apoderado> = async (data) => {
+        if (step == 1) {
+            setContrato(data as Contrato);
+            setStep(2);
+            methods.reset();
         } 
+        else if (step == 2) {
+            setApoderado(data as Apoderado);
+            try {
+                //add rut from apoderado to contrato data
+                if (!isValid) {
+                    methods.setError("rut", {
+                        type: "manual",
+                        message: "Rut invalido",
+                    });
+                }
+                if (contrato && isValid){
+                    contrato.rut = data.rut
+                    await createApoderado.mutateAsync(data as Apoderado);
+                    await createContrato.mutateAsync(contrato as Contrato);
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }else{
+            return;
+        }
     }
 
 
     return (
-            <Form methods={methods} onSubmit={onSubmit} className="flex flex-col m-5">
-                {({ register, setValue, formState: { errors } }) => {
+        <Form methods={methods} onSubmit={onSubmit} className="flex flex-col m-5">
+            {({ register, setValue, formState: { errors } }) => {
+                if (step === 1) {
                     return (
                         <>
                             <input type="text" placeholder="Nombre" {...register("nombre")} />
                             <input type="text" placeholder="Descripción" {...register("descripcion")} />
                             <input type="datetime-local" placeholder="Fecha de inicio" {...register("fechaInicio")} />
                             <input type="datetime-local" placeholder="Fecha de termino" {...register("fechaTermino")} />
-                            <input className="btn" type="submit" value="Enviar" />
+                            <button className="btn" type="submit">Next</button>
                         </>
                     );
-                }}
-            </Form>
+                } else if (step === 2) {
+                    return (
+                        <>
+                            {errors.rut && <p>{errors.rut.message}</p>}
+                            <input type="text" placeholder="Nombre" {...register("nombre")} />
+                            <input type="text" placeholder="Apellido" {...register("apellido")} />
+                            <input type="text" placeholder="Telefono" {...register("telefono")} />
+                            <input type="text" placeholder="Correo" {...register("correo")} />
+                            <input  
+                                value={rut.formatted} 
+                                type="text" 
+                                placeholder="Rut" 
+                                {...register("rut",{
+                                    onChange:(e) => {
+                                        updateRut(e.target.value);
+                                        setValue("rut", e.target.value);
+                                    }
+                                })} 
+                            />                            
+                            <button className="btn" type="submit">Create</button>
+                        </>
+                    );
+                }
+            }}
+        </Form>
     )
 }
