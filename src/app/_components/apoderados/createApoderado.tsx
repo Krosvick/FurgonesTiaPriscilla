0b4912a4dce6d7e1394316402b7259d9a0f12795
José Sandoval <josesand99v@gmail.com>
2023-12-18 07:21:11 +0000

    Refactor form in CreateApoderado component

--- src/app/_components/apoderados/createApoderado.tsx
+++ src/app/_components/apoderados/createApoderado.tsx
@@ -6,27 +6,22 @@ import { api } from "~/trpc/react";
 import { useRouter } from "next/navigation";
 import { apoderadoSchema } from "~/server/zodTypes/apoderadoTypes";
 import { useEffect } from "react";
+import { useRut } from "react-rut-formatter";
+import Form from "~/app/_components/Form"
 
 type Apoderado = z.infer<typeof apoderadoSchema>;
 
 export function CreateApoderado() {
-    const { data: apoderadoData, isLoading } = api.apoderados.getLatest.useQuery();
-    const router = useRouter();
 
-    const { register, handleSubmit, formState: {errors}, setValue  } = useForm<Apoderado>({
+    const methods = useForm<Apoderado>({
         resolver: zodResolver(apoderadoSchema),
     });
 
-    useEffect(() => {
-        if(!isLoading && apoderadoData) {
-            setValue("nombre", apoderadoData.nombre);
-            setValue("apellido", apoderadoData.apellido);
-            setValue("telefono", apoderadoData.telefono);
-            setValue("correo", apoderadoData.correo);
-            setValue("rut", apoderadoData.rut);
-        }
-    }
-    , [apoderadoData, isLoading, setValue]);
+    const { rut, updateRut, isValid } = useRut();
+    const { data: apoderadoData, isLoading } = api.apoderados.getLatest.useQuery();
+    const router = useRouter();
+
+    
 
     const createApoderado = api.apoderados.create.useMutation({
         onSuccess: () => {
@@ -35,7 +30,19 @@ export function CreateApoderado() {
     });
 
     const onSubmit: SubmitHandler<Apoderado> = async (data) => {
-        await createApoderado.mutateAsync(data);
+       try{
+            if (!isValid) {
+                methods.setError("rut", {
+                    type: "manual",
+                    message: "Rut invalido",
+                });
+                console.log("rut invalido");
+            }else{
+            await createApoderado.mutateAsync(data);
+            }
+       } catch (error) {
+              console.log(error);
+        } 
     }
 
     if (isLoading) {
@@ -44,14 +51,40 @@ export function CreateApoderado() {
 
     return (
         <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input type="text" placeholder="Nombre" {...register("nombre")} />
-                <input type="text" placeholder="Apellido" {...register("apellido")} />
-                <input type="text" placeholder="Telefono" {...register("telefono")} />
-                <input type="text" placeholder="Correo" {...register("correo")} />
-                <input type="text" placeholder="rut" {...register("rut")} />                            
-                <input type="submit" value="Enviar" />
-            </form>
+            <Form methods={methods} onSubmit={onSubmit} className="">
+                {({ register, setValue, formState: { errors } }) => {
+                    useEffect(() => {
+                        if(!isLoading && apoderadoData) {
+                            setValue("nombre", apoderadoData.nombre);
+                            setValue("apellido", apoderadoData.apellido);
+                            setValue("telefono", apoderadoData.telefono);
+                            setValue("correo", apoderadoData.correo);
+                        }
+                    }, [apoderadoData, isLoading, setValue]);
+
+                    return (
+                        <>
+                            {errors.rut && <p>{errors.rut.message}</p>}
+                            <input type="text" placeholder="Nombre" {...register("nombre")} />
+                            <input type="text" placeholder="Apellido" {...register("apellido")} />
+                            <input type="text" placeholder="Telefono" {...register("telefono")} />
+                            <input type="text" placeholder="Correo" {...register("correo")} />
+                            <input  
+                                value={rut.formatted} 
+                                type="text" 
+                                placeholder="Rut" 
+                                {...register("rut",{
+                                    onChange:(e) => {
+                                        updateRut(e.target.value);
+                                        setValue("rut", e.target.value);
+                                    }
+                                })} 
+                            />                            
+                            <input className="btn" type="submit" value="Enviar" />
+                        </>
+                    );
+                }}
+            </Form>
         </div>
     )
 }
