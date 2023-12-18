5bc4bb7b923390424db9856a5290ed268714d7fe
José Sandoval <josesand99v@gmail.com>
2023-12-18 07:20:12 +0000

    Add create method to ContratosRouter

--- src/server/api/routers/contratos.ts
+++ src/server/api/routers/contratos.ts
@@ -1,6 +1,24 @@
 import { z } from "zod";
 
 import { adminProcedure, createTRPCRouter, protectedProcedure, publicProcedure } from "~/server/api/trpc";
+import { contratoSchema } from "~/server/zodTypes/contratoTypes";
 
 export const ContratosRouter = createTRPCRouter({
+    create: adminProcedure
+        .input(contratoSchema)
+        .mutation(async ({ ctx, input }) => {
+        return ctx.db.contratos.create({
+            data: {
+                nombre: input.nombre,
+                descripcion: input.descripcion,
+                fechaInicio: input.fechaInicio,
+                fechaTermino: input.fechaTermino,
+                Apoderado: {
+                    connect: {
+                        rut: input.rut,
+                    },
+                },
+            },
+        });
+    }),
 });
