842b6f6082ea46bafd09fb65f0bc339ea293d511
José Sandoval <josesand99v@gmail.com>
2023-12-21 03:51:04 +0000

    Add PagoConfirmadoPage component

---
+++ src/app/pagar/[id]/confirmado/page.tsx
@@ -0,0 +1,13 @@
+
+export default function PagoConfirmadoPage() {
+  return (
+    <section className="min-w-screen flex min-h-screen justify-center p-5">
+        <div className="flex flex-col justify-center items-center">
+            <h1 className="text-4xl font-bold">Pago Confirmado</h1>
+            <p className="text-lg">
+            Su pago ha sido confirmado, gracias por su preferencia.
+            </p>
+        </div>
+    </section>
+  );
+}
