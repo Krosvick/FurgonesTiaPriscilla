67acb2142bdd7285e7270d3c3e4dbbbf28021342
José Sandoval <josesand99v@gmail.com>
2023-12-22 06:34:07 +0000

    Add Button component to Navbar

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { SignedIn, UserButton, SignedOut, SignInButton } from "@clerk/nextjs";
+import { Button } from "@nextui-org/react";
 
 interface NavbarProps {
   logo?: string;
@@ -16,12 +17,14 @@ export function Navbar({ logo, title, elements = [], showAuthInfo, className }: 
       //add classname props to nav
       <nav className={`navbar mx-auto py-5 shadow-lg bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-80 ${className}`}>
         <div className="flex-1">
-          {logo && <Image src={logo} alt="Logo" />}
+          {logo && <Image src={logo} alt="Logo" width={100} height={100} />}
           <Link href="/" className="btn btn-ghost text-xl">{title}</Link>
         </div>
         <div className="dropdown dropdown-end">
-          <div tabIndex={0} role="button" className="btn btn-ghost rounded-btn lg:hidden">Dropdown</div>
-          <ul tabIndex={0} className="menu dropdown-content lg:menu-horizontal flex items-center px-1">
+          <Button tabIndex={0} role="button" isIconOnly className="lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M256 0a256 256 0 1 0 0 512A256 256 0 1 0 256 0zM135 241c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l87 87 87-87c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9L273 345c-9.4 9.4-24.6 9.4-33.9 0L135 241z"/></svg>
+          </Button>
+          <ul tabIndex={0} className="menu dropdown-content lg:menu-horizontal flex items-center px-1 bg-gray-100 outline outline-2 rounded-lg gap-5">
             {elements?.map((element, index) => (
               <li key={index}>{element}</li>
             ))}
@@ -44,7 +47,7 @@ export function Navbar({ logo, title, elements = [], showAuthInfo, className }: 
             ))}
             {showAuthInfo ? 
               <div className="mx-5">
-                <div className="scale-150">
+                <div className="scale-150 mx-5">
                   <SignedIn><UserButton /></SignedIn>
                 </div>
                 <SignedOut>
