918e762271e0a3a11d2bea9ae59373ad9da26a4f
José Sandoval <josesand99v@gmail.com>
2023-12-24 05:47:27 +0000

    Remove and add link to Furgones in AdminLayout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -29,8 +29,8 @@ export default function AdminLayout(props: AdminLayoutProps) {
                     <Link href="/admin">Inicio</Link>,
                     <Link href="/admin/contratos">Contratos</Link>,
                     <Link href="/admin/apoderados">Apoderados</Link>,
-                    <Link href="/admin/furgones">Furgones</Link>,
                     <Link href="/admin/pagos">Pagos</Link>,
+                    <Link href="/admin/furgones">Furgones</Link>,
 
                 ]}
                 sidebarButton={drawerButton}
