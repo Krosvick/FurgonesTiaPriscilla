a021b311476e75481ab237d20aca988a267056c7
José Sandoval <josesand99v@gmail.com>
2023-12-24 05:47:52 +0000

    Refactor ContratoCard component

--- src/app/_components/dataDisplay/ContratoCard.tsx
+++ src/app/_components/dataDisplay/ContratoCard.tsx
@@ -1,35 +1,36 @@
 import { Card, CardBody } from '@nextui-org/react';
-import { Contratos as PrismaContrato, Apoderados } from '@prisma/client';
 import { PagosModal } from "../contratos/PagosModals";
+import { UpdateContratoModal } from '../contratos/UpdateContratoModal';
+import { contratoUpdateSchema } from '~/server/zodTypes/contratoTypes';
+import * as z from 'zod';
 
-type Contrato = PrismaContrato & {
-  Apoderado: Apoderados | null;
-};
+type Contrato = z.infer<typeof contratoUpdateSchema>;
 
 export function ContratoCard({ contrato }: { contrato: Contrato }) {
     return(
-        <Card className="w-full bg-slate-100">
-            <CardBody className='flex flex-row px-5 py-10'>
-                <div className="w-3/4 flex flex-col items-start justify-start">
-                    <div className="w-full flex flex-row justify-between">
-                        <div className="w-1/3">
-                            <span className="text-gray-500">Contrato</span>
-                            <span className="text-gray-800">{contrato.nombre}</span>
+        <Card className="w-full bg-slate-100 rounded-lg shadow-lg overflow-hidden">
+            <CardBody className='flex flex-col md:flex-row px-5 py-10'>
+                <div className="w-full md:w-3/4 flex flex-col items-start justify-start">
+                    <div className="w-full flex flex-col md:flex-row justify-between mb-4">
+                        <div className="w-full md:w-1/3 mb-4 md:mb-0">
+                            <h2 className="text-gray-500 text-sm uppercase tracking-wide">Contrato</h2>
+                            <h1 className="text-gray-800 text-lg font-bold">{contrato.nombre}</h1>
                         </div>
-                        <div className="w-1/3">
-                            <span className="text-gray-500">Descripción</span>
-                            <span className="text-gray-800">{contrato.descripcion}</span>
+                        <div className="w-full md:w-1/3">
+                            <h2 className="text-gray-500 text-sm uppercase tracking-wide">Descripción</h2>
+                            <p className="text-gray-800 text-lg">{contrato.descripcion}</p>
                         </div>
                     </div>
                     <div className="w-full flex flex-row justify-between">
-                        <div className="w-1/3">
-                            <span className="text-gray-500">Cliente</span>
-                            <span className="text-gray-800">{contrato.Apoderado?.apellido}</span>
+                        <div className="w-full md:w-1/3">
+                            <h2 className="text-gray-500 text-sm uppercase tracking-wide">Cliente</h2>
+                            <p className="text-gray-800 text-lg">{contrato.Apoderado?.nombre} {contrato.Apoderado?.apellido}</p>
                         </div>
                     </div>
                 </div>
-                <div className="flex justify-center items-center w-1/4">
+                <div className="flex justify-end items-center w-full md:w-1/4 mt-4 md:mt-0">
                     <PagosModal idContrato={contrato.idContrato} />
+                    <UpdateContratoModal contrato={contrato} />
                 </div>
             </CardBody>
         </Card>
