83d297f9c618d2ba4da83b03f72dc0d743eacb04
José Sandoval <josesand99v@gmail.com>
2023-12-24 05:46:53 +0000

    Update PupilosModal to CreatePupilosModal and add UpdatePupilosModal

--- src/app/admin/contratos/[id]/page.tsx
+++ src/app/admin/contratos/[id]/page.tsx
@@ -3,11 +3,13 @@ import { Container } from "~/app/_components/Container";
 import { api } from "~/trpc/react";
 import { ContratoCard } from "~/app/_components/dataDisplay/ContratoCard";
 import { Card } from "~/app/_components/Card";
-import { PupilosModal } from "~/app/_components/pupilos/PupilosModal";
+import { CreatePupilosModal } from "~/app/_components/pupilos/CreatePupilosModal";
+import { UpdatePupilosModal } from "~/app/_components/pupilos/UpdatePupilosModal";
 
 export default function Page({ params }: { params: { id: string } }) {
     const {data: contrato, isLoading, isFetched} = api.contratos.getById.useQuery(params.id);
-    const {data:pupilos, isLoading:isLoadingPupilos, isFetched:isFetchedPupilos} = api.pupilos.getAll.useQuery();
+    const { data: pupilos, isLoading: isLoadingPupilos, isFetched: isFetchedPupilos } = api.pupilos.getByApoderadoId.useQuery(contrato?.Apoderado?.idApoderado || '', {
+    enabled: !!contrato?.Apoderado,});
 
     if (isLoading) {
         return <div>Loading...</div>;
@@ -20,17 +22,21 @@ export default function Page({ params }: { params: { id: string } }) {
                     {isFetched && contrato && <ContratoCard contrato={contrato} />}
                 </div>
                 <div className="mt-5 mb-1">
-                    {contrato && contrato.Apoderado && <PupilosModal idApoderado={contrato.Apoderado.idApoderado} idContrato={contrato.idContrato} />}
+                    {contrato && contrato.Apoderado && <CreatePupilosModal idApoderado={contrato.Apoderado.idApoderado} idContrato={contrato.idContrato} />}
                 </div>
-                <Card dark={false}>
                     {isFetchedPupilos && pupilos && pupilos.map((pupilo) => (
-                        <div key={pupilo.idPupilo} className="p-4 border-b-2 border-gray-200">
-                            <h2 className="text-xl font-bold">Nombre: {pupilo.nombre} {pupilo.apellido}</h2>
-                            <p className="text-gray-600">Rut: {pupilo.rut}</p>
-                            <p className="text-gray-600">Colegio: {pupilo.colegio}</p>
-                        </div>
+                    <Card dark={false} className="flex justify-between">
+                            <div key={pupilo.idPupilo} className="p-4 w-3/4">
+                                <h2 className="text-xl font-bold">Nombre: {pupilo.nombre} {pupilo.apellido}</h2>
+                                <p className="text-gray-600">Rut: {pupilo.rut}</p>
+                                <p className="text-gray-600">Colegio: {pupilo.colegio}</p>
+                                <p className="text-gray-600">Tipo de contrato: {pupilo.detalle?.tipo === "ida" ? "Ida" : pupilo.detalle?.tipo === "vuelta" ? "Vuelta" : "Ida y vuelta"}</p>
+                            </div>
+                            <div className="flex items-center">
+                                <UpdatePupilosModal pupilo={pupilo} />
+                            </div>
+                    </Card>
                     ))}
-                </Card>
             </Container>
         </section>
     )
