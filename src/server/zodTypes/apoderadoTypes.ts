f8ba91dc22600cbd4c533858d0d7844d02c3aba9
Krosvick <josesand99v@gmail.com>
2023-12-24 22:38:31 -0300

    Add updateApoderadoSchema to apoderadoTypes.ts

--- src/server/zodTypes/apoderadoTypes.ts
+++ src/server/zodTypes/apoderadoTypes.ts
@@ -7,3 +7,14 @@ export const apoderadoSchema = z.object({
     correo: z.string().email(),
     rut: z.string(),
 });
+
+export const updateApoderadoSchema = z.object({
+    idApoderado: z.string().uuid(),
+    nombre: z.string(),
+    apellido: z.string(),
+    rut: z.string(),
+    telefono: z.string(),
+    correo: z.string().email(),
+    idContrato: z.string().uuid().nullable(),
+    CreatedAt: z.coerce.date(),
+});
