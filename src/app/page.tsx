d9c24dc162706d7bef2d06719747f4b1d5abce4f
Krosvick <josesand99v@gmail.com>
2023-12-26 01:06:41 -0300

    Refactor social media link styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,10 +42,14 @@ export default async function Home() {
 
               <div className="mt-6 lg:mt-12">
                 <span className="text-xs font-medium text-gray-800 uppercase dark:text-gray-200">Nuestras redes:</span>
-                <Link href="https://web.facebook.com/profile.php?id=100054458799644" className="flex items-center mt-3 text-gray-900 dark:text-gray-100 hover:text-gray-700 dark:hover:text-gray-400">
-                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48">
-                    <path fill="#039be5" d="M24 5A19 19 0 1 0 24 43A19 19 0 1 0 24 5Z"></path><path fill="#fff" d="M26.572,29.036h4.917l0.772-4.995h-5.69v-2.73c0-2.075,0.678-3.915,2.619-3.915h3.119v-4.359c-0.548-0.074-1.707-0.236-3.897-0.236c-4.573,0-7.254,2.415-7.254,7.917v3.323h-4.701v4.995h4.701v13.729C22.089,42.905,23.032,43,24,43c0.875,0,1.729-0.08,2.572-0.194V29.036z"></path>
-                    </svg>
+                <Link 
+                  href="https://web.facebook.com/profile.php?id=100054458799644" 
+                  className="flex items-center mt-3 text-gray-900 dark:text-gray-100 hover:text-gray-700 dark:hover:text-gray-400 transition duration-300 ease-in-out transform hover:scale-110 origin-center"
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48">
+                    <path fill="#039be5" d="M24 5A19 19 0 1 0 24 43A19 19 0 1 0 24 5Z"></path>
+                    <path fill="#fff" d="M26.572,29.036h4.917l0.772-4.995h-5.69v-2.73c0-2.075,0.678-3.915,2.619-3.915h3.119v-4.359c-0.548-0.074-1.707-0.236-3.897-0.236c-4.573,0-7.254,2.415-7.254,7.917v3.323h-4.701v4.995h4.701v13.729C22.089,42.905,23.032,43,24,43c0.875,0,1.729-0.08,2.572-0.194V29.036z"></path>
+                </svg>
                 </Link>
 
               </div>
